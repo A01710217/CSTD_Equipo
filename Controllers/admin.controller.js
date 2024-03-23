0b0ed8bf7c508c0352110eb37d638e5396ba8c29
Benjamin Arauz <benjaminarauzc@gmail.com>
2024-03-23 01:40:43 -0600

    Pruebas hechas de agregar preguntas

--- Controllers/admin.controller.js
+++ Controllers/admin.controller.js
@@ -28,14 +28,15 @@ exports.get_configurar_email = (request, response, next) => {
 }
 
 exports.get_preguntas_marca = (request, response, next) => {
+    let informacion = request.session.informacion || "";
+    request.session.informacion = "";
     Pregunta.fetchByMarca(request.params.marca_id).then(([preguntas, fieldData]) => {
         Opcion.fetchByMarca(request.params.marca_id).then(([opciones, fieldData]) => {
-            //console.log(preguntas);
-            //console.log(opciones);
             response.render("preguntas", {
                 preguntas: preguntas,
                 opciones: opciones,
-                marca_id: request.params.marca_id
+                marca_id: request.params.marca_id,
+                informacion: informacion,
             });
         }).catch((error) => {
             console.log(error);
@@ -46,37 +47,60 @@ exports.get_preguntas_marca = (request, response, next) => {
 }
 
 exports.get_crear_pregunta = (request, response, next) => {
+    let error = request.session.error || "";
+    request.session.error = "";
+    let usuario_input = request.session.usuario_input || {};
+    request.session.usuario_input = {};
+    console.log(usuario_input);
     Tipo.fetchAll().then(([tipos, fieldData]) => {
         response.render("crear_pregunta", {
             tipos: tipos,
-            marca_id: request.params.marca_id
+            marca_id: request.params.marca_id, 
+            error: error,
+            usuario_input: usuario_input,
         });
     }).catch((error) => {
         console.log(error);
     });
 };
+
 exports.post_crear_pregunta = (request, response, next) => {
-    Tipo.fetchOne(request.body.pregunta_tipo).then(([tipo, fieldData]) => {
-        request.body.pregunta_obligatoria === "on" ? pregunta_obligatoria = 1 : pregunta_obligatoria = 0;
-        //Crear la pregunta
-        let pregunta = new Pregunta(request.params.marca_id, request.body.pregunta_texto, tipo[0].idtipo, pregunta_obligatoria);
-        //Guardar la pregunta en la base de datos
-        pregunta.save().then(() => {
-            Pregunta.fetchByDate().then(([preguntas, fieldData]) => {
-                for (let opcion of request.body.pregunta_opcion) {
-                    let nueva_opcion = new Opcion(preguntas[0].idpregunta, opcion);
-                    nueva_opcion.save();
-                }
-                response.redirect("/admin/configurar_email/preguntas/" + request.params.marca_id);
+    // Revisar si existe más de una opción
+    if (Array.isArray(request.body.pregunta_opcion) === false){
+        request.session.error = "Porfavor ingresar más de una opción";
+        request.session.usuario_input = request.body;
+        response.redirect("/admin/configurar_email/crear-pregunta/" + request.params.marca_id);
+    } else if (request.body.pregunta_texto === "") {
+        request.session.error = "Porfavor ingresar un texto para la pregunta";
+        request.session.usuario_input = request.body;
+        response.redirect("/admin/configurar_email/crear-pregunta/" + request.params.marca_id);
+    } else {
+        // Busca los tipos de preguntas
+        Tipo.fetchOne(request.body.pregunta_tipo).then(([tipo, fieldData]) => {
+            request.body.pregunta_obligatoria === "on" ? pregunta_obligatoria = 1 : pregunta_obligatoria = 0;
+            //Crear la pregunta
+            let pregunta = new Pregunta(request.params.marca_id, request.body.pregunta_texto, tipo[0].idtipo, pregunta_obligatoria);
+            //Guardar la pregunta en la base de datos
+            pregunta.save().then(() => {
+                Pregunta.fetchByDate().then(([preguntas, fieldData]) => {
+                    for (let opcion of request.body.pregunta_opcion) {
+                        let nueva_opcion = new Opcion(preguntas[0].idpregunta, opcion);
+                        nueva_opcion.save().then(() => {}).catch((error) => {
+                            console.log(error);
+                        });
+                    }
+                    request.session.informacion = "Pregunta creada exitosamente";
+                    response.redirect("/admin/configurar_email/preguntas/" + request.params.marca_id);
+                }).catch((error) => {
+                    console.log(error);
+                });
             }).catch((error) => {
                 console.log(error);
             });
         }).catch((error) => {
             console.log(error);
         });
-    }).catch((error) => {
-        console.log(error);
-    });
+    }
     //let pregunta = new Pregunta(request.params.marca_id, request.body.pregunta_texto, request.body.idtipo, request.body.statusobligatorio);
 };
 
